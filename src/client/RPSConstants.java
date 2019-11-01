6313b0da35b87878f4efbd1f3d62d807bce284cc
Anna <ajuels18@student.aau.dk>
2019-11-01 15:18:33 +0100

    client and rpsconstants

---
+++ src/client/RPSConstants.java
@@ -0,0 +1,18 @@
+package client;
+
+
+public interface RPSConstants {
+	
+	public static int P1WON = 1;
+	public static int P2WON = 2;
+	public static int TIE = 3;
+	public static int CONT = 4;
+	
+	public static int PLAYER1 = 1;
+	public static int PLAYER2 = 2;
+	//public static int player3 = 3;
+	
+	public static char rock = 'r';
+	public static char paper = 'p';
+	public static char scissor = 's';
+}
